65ac2fa3aa9ba722e554d28b4ca92bef90a79909
Miwa <63481257+ensan-hcl@users.noreply.github.com>
2025-03-30 16:48:17 +0900

    Update `testConvertToTimeExpression` function to use `await` for `convertToTimeExpression` calls
    * Ensure `convertToTimeExpression` function is called asynchronously in all test cases

--- Tests/KanaKanjiConverterModuleTests/ConverterTests/TimeExpressionTests.swift
+++ Tests/KanaKanjiConverterModuleTests/ConverterTests/TimeExpressionTests.swift
@@ -14,16 +14,16 @@ final class TimeExpressionTests: XCTestCase {
         let converter = await KanaKanjiConverter()
 
         // Test 3-digit numbers
-        XCTAssertEqual(converter.convertToTimeExpression(makeDirectInput(direct: "123")).first?.text, "1:23")
-        XCTAssertEqual(converter.convertToTimeExpression(makeDirectInput(direct: "945")).first?.text, "9:45")
-        XCTAssertEqual(converter.convertToTimeExpression(makeDirectInput(direct: "760")).first?.text, "7:60")
-        XCTAssertTrue(converter.convertToTimeExpression(makeDirectInput(direct: "761")).isEmpty) // Invalid minute
+        XCTAssertEqual(await converter.convertToTimeExpression(makeDirectInput(direct: "123")).first?.text, "1:23")
+        XCTAssertEqual(await converter.convertToTimeExpression(makeDirectInput(direct: "945")).first?.text, "9:45")
+        XCTAssertEqual(await converter.convertToTimeExpression(makeDirectInput(direct: "760")).first?.text, "7:60")
+        XCTAssertTrue(await converter.convertToTimeExpression(makeDirectInput(direct: "761")).isEmpty) // Invalid minute
 
         // Test 4-digit numbers
-        XCTAssertEqual(converter.convertToTimeExpression(makeDirectInput(direct: "1234")).first?.text, "12:34")
-        XCTAssertEqual(converter.convertToTimeExpression(makeDirectInput(direct: "9450")).first?.text, "09:45")
-        XCTAssertEqual(converter.convertToTimeExpression(makeDirectInput(direct: "7600")).first?.text, "07:60")
-        XCTAssertTrue(converter.convertToTimeExpression(makeDirectInput(direct: "1360")).isEmpty) // Invalid hour
-        XCTAssertTrue(converter.convertToTimeExpression(makeDirectInput(direct: "1261")).isEmpty) // Invalid minute
+        XCTAssertEqual(await converter.convertToTimeExpression(makeDirectInput(direct: "1234")).first?.text, "12:34")
+        XCTAssertEqual(await converter.convertToTimeExpression(makeDirectInput(direct: "9450")).first?.text, "09:45")
+        XCTAssertEqual(await converter.convertToTimeExpression(makeDirectInput(direct: "7600")).first?.text, "07:60")
+        XCTAssertTrue(await converter.convertToTimeExpression(makeDirectInput(direct: "1360")).isEmpty) // Invalid hour
+        XCTAssertTrue(await converter.convertToTimeExpression(makeDirectInput(direct: "1261")).isEmpty) // Invalid minute
     }
 }
