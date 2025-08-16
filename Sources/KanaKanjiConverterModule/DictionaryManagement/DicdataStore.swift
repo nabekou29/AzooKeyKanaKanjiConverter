45ce7521ed211a4fa718cf209a22a0f70e5e1593
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2025-08-17 08:33:16 +0900

    fix: 不整合を発見したので修正

--- Sources/KanaKanjiConverterModule/DictionaryManagement/DicdataStore.swift
+++ Sources/KanaKanjiConverterModule/DictionaryManagement/DicdataStore.swift
@@ -660,7 +660,7 @@ public final class DicdataStore {
         convertTarget: String,
         surfaceRange: Range<Int>,
         fullText: [Character],
-        keyboardLanguage _: KeyboardLanguage
+        keyboardLanguage: KeyboardLanguage
     ) -> [DicdataElement] {
         var result: [DicdataElement] = []
         result.append(contentsOf: self.getJapaneseNumberDicdata(head: convertTarget))
@@ -678,7 +678,7 @@ public final class DicdataStore {
             }
         }
         // convertTargetを英単語として候補に追加する
-        if convertTarget.onlyRomanAlphabet {
+        if convertTarget.onlyRomanAlphabet && keyboardLanguage == .en_US {
             result.append(DicdataElement(ruby: convertTarget, cid: CIDData.固有名詞.cid, mid: MIDData.英単語.mid, value: -14))
         }
         // convertTargetが1文字のケースでは、ひらがな・カタカナに変換したものを候補に追加する
